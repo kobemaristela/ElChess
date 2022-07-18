ce7b53b8653bc31f89d1ebb58c43e72421f4affa
Kobe Maristela <kobe@maristela.net>
2022-07-17 23:55:52 -0700

    Added csvparser multiprocess

--- Puzzles/csvparser.py
+++ Puzzles/csvparser.py
@@ -1,4 +1,25 @@
-import faster_than_csv as faster_than_csv
+import pandas as pd
+import multiprocessing as mp
+from fenparser import FenParser
 
+class CsvParse():
+    def __init__(self, csvFile, workers=4, chunksize=1000):
+        self.csv = csvFile
+        self.workers = workers
+        self.chunksize = chunksize
 
+    def parse(self):
+        read = pd.read_table(self.csv, chunksize=self.chunksize)
+        worker = mp.Pool(self.workers)
 
+        procs = []
+
+        for line in read:
+            f = worker.apply_async(proc,[line])
+            procs.append(f)
+
+    def proc(self, fen):
+        data = FenParser(fen)
+
+if __name__ == "__main__":
+    file_path = "TEST PATH"
