e00efcd0d35fd117bd0db7041441ede4b7a29177
Kobe Maristela <kobe@maristela.net>
2022-07-19 23:52:51 -0700

    Added parquet conversion

--- Puzzles/csvparser.py
+++ Puzzles/csvparser.py
@@ -1,25 +1,34 @@
 import pandas as pd
 import multiprocessing as mp
 from fenparser import FenParser
+from pyarrow import csv as arrow
 
 class CsvParse():
-    def __init__(self, csvFile, workers=4, chunksize=1000):
-        self.csv = csvFile
+    def __init__(self, csv_file, workers=4, chunksize=101):
+        self.csv = csv_file
         self.workers = workers
         self.chunksize = chunksize
+    
+    def pyarrow_parse(self):
+        read_options = arrow.ReadOptions(autogenerate_column_names=True)
+        read = arrow.read_csv(self.csv, read_options=read_options)
+        print(read)
 
     def parse(self):
-        read = pd.read_table(self.csv, chunksize=self.chunksize)
+        read = pd.read_csv(self.csv, engine="pyarrow")
         worker = mp.Pool(self.workers)
 
         procs = []
 
         for line in read:
-            f = worker.apply_async(proc,[line])
+            f = worker.apply_async(self.proc,[line])
             procs.append(f)
 
     def proc(self, fen):
+        print(fen)
         data = FenParser(fen)
 
 if __name__ == "__main__":
-    file_path = "TEST PATH"
+    file_path = r"C:\Users\Kobe\Desktop\projects\project-1\Puzzles\lichess_db_puzzle.csv"
+    p = CsvParse(file_path)
+    p.pyarrow_parse()
