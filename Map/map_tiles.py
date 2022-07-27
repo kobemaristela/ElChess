273e2389844e02d35285373dedb07d33fcc2ccb9
Jordan Rood <jrood2024@nevada.unr.edu>
2022-07-26 23:04:26 -0700

    Player movement functions and boss pic added

--- Map/map_tiles.py
+++ Map/map_tiles.py
@@ -1,3 +1,4 @@
+from importlib.resources import path
 import pygame
 from map_constants import *
 
@@ -7,11 +8,45 @@ class Wall(pygame.sprite.Sprite):
         self.image = pygame.image.load('wall_mid.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = position)
 
+class Boss(pygame.sprite.Sprite):
+    def __init__(self, position, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load('boss_pic.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft = position)
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
         self.image = pygame.image.load('knight_player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = position)
+
+        self.direction = pygame.math.Vector2()
+        self.speed = 3
+
+    def keyboard_input(self):
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[pygame.K_UP]:
+            self.direction.y = -1
+        elif key_pressed[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+        
+        if key_pressed[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif key_pressed[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+        self.rect.center += self.direction * speed
+
+    def update(self):
+        self.keyboard_input()
+        self.move(self.speed)
 
 class Map:
     def __init__(self):
@@ -30,8 +65,9 @@ class Map:
                     Wall((x, y), [self.visible_sprites, self.obstacle_sprites])
                 elif col == 'p':
                     self.player = Player((x, y), [self.visible_sprites])
-                # elif col == 'B':
-                #    Boss((x, y), [self.visible_sprites])
+                elif col == 'B':
+                   Boss((x, y), [self.visible_sprites, self.obstacle_sprites])
                 
     def run(self):
         self.visible_sprites.draw(self.display_screen)
+        self.visible_sprites.update()
