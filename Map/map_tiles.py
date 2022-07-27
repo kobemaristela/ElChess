bcd98b9f8822db576218268989170d129230d3b2
Jordan Rood <jrood2024@nevada.unr.edu>
2022-07-27 16:25:49 -0700

    Player can now move around map while being centered on screen

--- Map/map_tiles.py
+++ Map/map_tiles.py
@@ -3,9 +3,20 @@ import pygame
 from map_constants import *
 
 class Wall(pygame.sprite.Sprite):
+    def __init__(self, position, groups, type):
+        super().__init__(groups)
+        if type == 'mid':
+            self.image = pygame.image.load('wall_mid.png').convert_alpha()
+        elif type == 'left':
+            self.image = pygame.image.load('wall_left.png').convert_alpha()
+        elif type == 'right':
+            self.image = pygame.image.load('wall_right.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft = position)
+
+class Floor(pygame.sprite.Sprite):
     def __init__(self, position, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('wall_mid.png').convert_alpha()
+        self.image = pygame.image.load('floor.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = position)
 
 class Boss(pygame.sprite.Sprite):
@@ -52,7 +63,7 @@ class Map:
     def __init__(self):
         self.display_screen = pygame.display.get_surface()
 
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = Player_Camera()
         self.obstacle_sprites = pygame.sprite.Group()
         self.make_map()
 
@@ -62,12 +73,30 @@ class Map:
                 x = col_index * TILE_SIZE
                 y = row_index * TILE_SIZE
                 if col == 'w':
-                    Wall((x, y), [self.visible_sprites, self.obstacle_sprites])
+                    Wall((x, y), [self.visible_sprites, self.obstacle_sprites], 'mid')
+                elif col == 'l':
+                    Wall((x, y), [self.visible_sprites, self.obstacle_sprites], 'left')  
+                elif col == 'r':
+                    Wall((x, y), [self.visible_sprites, self.obstacle_sprites], 'right')              
                 elif col == 'p':
                     self.player = Player((x, y), [self.visible_sprites])
                 elif col == 'B':
                    Boss((x, y), [self.visible_sprites, self.obstacle_sprites])
                 
     def run(self):
-        self.visible_sprites.draw(self.display_screen)
+        self.visible_sprites.center_camera_draw(self.player)
         self.visible_sprites.update()
+
+class Player_Camera(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_screen = pygame.display.get_surface()
+        self.vector_to_keep_player_center = pygame.math.Vector2()
+
+    def center_camera_draw(self, player):
+        self.vector_to_keep_player_center.x = player.rect.centerx - (WIDTH // 2)
+        self.vector_to_keep_player_center.y = player.rect.centery - (HEIGHT // 2)
+
+        for sprite in self.sprites():
+            camera_position = sprite.rect.topleft - self.vector_to_keep_player_center
+            self.display_screen.blit(sprite.image, camera_position)
