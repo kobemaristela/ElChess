8d4c8149f8d12e6c912dac085f14f73da09c09cd
Kobe Maristela <kobe@maristela.net>
2022-08-06 21:50:03 -0700

    Added game options

--- Chess/game.py
+++ Chess/game.py
@@ -4,19 +4,34 @@ from constants import *
 
 
 class ChessGame():
-    def __init__(self, type, difficulty, ):
+    def __init__(self, game_type, difficulty, database=None):
         # Initialize game
         pygame.display.init()
         pygame.font.init()
 
 
+        # Initialize game settings
+        self.set_game_type(game_type)
+        self.set_difficulty(difficulty)
 
-        #
+        self.database = database
+
 
     def set_difficulty(self, difficulty: str) -> None:
-        rating = {"easy": 800, "normal": 1100, "hard": 1400}
-        
+        rating = {"easy": 800, "normal": 1100, "hard": 1400}    # Set difficulty rating for puzzles
+
         if difficulty not in rating.keys():
-            raise ValueError("Invalid Difficulty: Only accept easy, normal, and hard")
+            raise ValueError(f"Invalid Difficulty: {difficulty}")
         
         self.rating = rating[difficulty]
+
+
+    def set_game_type(self, game_type):
+        if game_type not in ['normal', 'puzzle']:
+            raise ValueError(f"Invalid Game Type: {game_type}")
+
+        self.game_type = game_type
+    
+
+    def initialize_database(self, database):
+        
